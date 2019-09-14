d30eff4bc5502df226c2488f47f270869d1549b6
Vadim Sedov <sidovsky.develop@gmail.com>
2019-09-15 00:14:37 +0700

    Add solution files

---
+++ PDFView-Android.Sample/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace PDFViewAndroid.Sample
+{
+    [Activity(
+        Label = "@string/app_name",
+        Theme = "@style/AppTheme.NoActionBar",
+        MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            FindViewById<PDFView>(Resource.Id.activityMainPdfView)
+                .FromAsset("great-expectations.pdf")
+                .Show();
+        }
+    }
+}
